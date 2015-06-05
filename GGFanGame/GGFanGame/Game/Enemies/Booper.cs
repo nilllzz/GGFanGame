e9aa9d5a07b71f6db891a32edfe8ae65d5da2ccd
Mak91 <msholson91@gmail.com>
2015-06-05 16:17:05 -0400

    Added updated Booper2 sprite and animations

--- GGFanGame/GGFanGame/Game/Enemies/Booper.cs
+++ GGFanGame/GGFanGame/Game/Enemies/Booper.cs
@@ -13,7 +13,7 @@ namespace GGFanGame.Game.Level.Enemies
     {
         public Booper(GGGame game) : base(game)
         {
-            spriteSheet = game.textureManager.getResource(@"Sprites\Booper");
+            spriteSheet = game.textureManager.getResource(@"Sprites\Booper2");
             drawShadow = true;
             shadowSize = 0.6d;
             strength = 0f;
@@ -21,10 +21,10 @@ namespace GGFanGame.Game.Level.Enemies
             state = ObjectState.Idle;
             size = new Vector3(40, 48, 10);
 
-            addAnimation(ObjectState.Idle, new Animation(10, Point.Zero, new Point(64, 64), 6));
-            addAnimation(ObjectState.Hurt, new Animation(1, new Point(0, 64), new Point(64, 64), 20, 1));
-            addAnimation(ObjectState.HurtFalling, new Animation(1, new Point(0, 64), new Point(64, 64), 20, 1));
-            addAnimation(ObjectState.Dead, new Animation(10, new Point(0, 128), new Point(64, 64), 3));
+            addAnimation(ObjectState.Idle, new Animation(6, Point.Zero, new Point(64, 64), 6));
+            addAnimation(ObjectState.Hurt, new Animation(7, new Point(0, 64), new Point(64, 64), 4));
+            addAnimation(ObjectState.HurtFalling, new Animation(7, new Point(0, 64), new Point(64, 64), 4));
+            addAnimation(ObjectState.Dead, new Animation(6, new Point(0, 128), new Point(64, 64), 4));
 
             health = 50;
         }

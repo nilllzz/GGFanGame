9fa7f80d3027324cb4e1f481d105e0f0b33bbb3e
nilllzz <nidresch@googlemail.com>
2016-10-18 06:19:28 +0200

    new ASCIIProvider and alphabets for new game screen

---
+++ GGFanGame/GGFanGame/ASCIIProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GGFanGame
+{
+    /// <summary>
+    /// Easy access to ASCII char ranges.
+    /// </summary>
+    internal static class ASCIIProvider
+    {
+        private static IEnumerable<char> GetCharsEnumerable(int start, int count)
+            => Enumerable.Range(start, count).Select(i => (char)i);
+
+        /// <summary>
+        /// Returns an amount of ASCII chars from a starting position.
+        /// </summary>
+        internal static char[] GetChars(int start, int count)
+            => GetCharsEnumerable(start, count).ToArray();
+
+        /// <summary>
+        /// Returns multiple ranges of ASCII chars concatenated into a single array.
+        /// </summary>
+        /// <param name="positions">The positions of the chars in the ASCII table. Item1 is the start index, Item2 the count.</param>
+        internal static char[] GetChars(Tuple<int, int>[] positions)
+        {
+            if (positions == null || positions.Length == 0)
+                return new char[0];
+            if (positions.Length == 1)
+                return GetChars(positions[0].Item1, positions[0].Item2);
+
+            IEnumerable<char> chars = GetCharsEnumerable(positions[0].Item1, positions[0].Item2);
+
+            for (int i = 1; i < positions.Length; i++)
+                chars = chars.Concat(GetCharsEnumerable(positions[i].Item1, positions[i].Item2));
+
+            return chars.ToArray();
+        }
+    }
+}
--- GGFanGame/GGFanGame/Screens/Menu/NewGameScreen.cs
+++ GGFanGame/GGFanGame/Screens/Menu/NewGameScreen.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using static GameProvider;
 
 namespace GGFanGame.Screens.Menu
 {
@@ -11,15 +14,47 @@ namespace GGFanGame.Screens.Menu
         private readonly MenuBackgroundRenderer _backgroundRenderer;
         private readonly Screen _preScreen;
 
+        private static readonly char[]
+            _upperCaseAlphabet =
+                ASCIIProvider.GetChars(new[] { new Tuple<int, int>(65, 26), new Tuple<int, int>(48, 10) }),
+            _lowerCaseAlphabet =
+                ASCIIProvider.GetChars(new[] { new Tuple<int, int>(97, 26), new Tuple<int, int>(48, 10) });
+        private static readonly char[][] _alphabets = { _upperCaseAlphabet, _lowerCaseAlphabet };
+        private static readonly int[]
+            _charsPerRow =
+                { 10, 10, 6, 10 };
+        private readonly SpriteFont _font;
+        private int _alphabetIndex = 0;
+
         public NewGameScreen(Screen preScreen, MenuBackgroundRenderer backgroundRenderer)
         {
             _preScreen = preScreen;
             _backgroundRenderer = backgroundRenderer;
+
+            _font = Content.Load<SpriteFont>("Fonts\\CartoonFont");
         }
 
         public override void Draw()
         {
             _backgroundRenderer.Draw();
+            DrawNameSelection();
+        }
+
+        private void DrawNameSelection()
+        {
+            var alphabet = _alphabets[_alphabetIndex];
+            int charIndex = 0;
+            int rowIndex = 0;
+
+            foreach (var row in _charsPerRow)
+            {
+                for (int i = 0; i < row; i++)
+                {
+                    var c = alphabet[charIndex++];
+                    GameInstance.SpriteBatch.DrawString(_font, c.ToString(), new Vector2(i * 32, rowIndex * 32), Color.White);
+                }
+                rowIndex++;
+            }
         }
 
         public override void Update()

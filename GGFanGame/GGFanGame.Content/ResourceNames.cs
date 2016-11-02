1e17642c849c84e6c1e7e6a969cc681e88c8eb59
nilllzz <nidresch@googlemail.com>
2016-11-02 20:06:53 +0100

    Project management
    - changed framework to 4.5
    - Added TT files to generate a resource list and the content proj file
    - referenced content project in main project

---
+++ GGFanGame/GGFanGame.Content/ResourceNames.cs
@@ -0,0 +1,114 @@
+﻿
+namespace GGFanGame.Content
+{
+    public static class Resources
+    {
+        
+        public static class Fonts
+        {
+            public const string CartoonFont = "Fonts/CartoonFont";
+            public const string CartoonFontLarge = "Fonts/CartoonFontLarge";
+            public const string CartoonFontSmall = "Fonts/CartoonFontSmall";
+        }
+        
+        public static class Levels
+        {
+            
+            public static class GrumpSpace
+            {
+                public const string Arcade0 = "Levels/GrumpSpace/Arcade0";
+                public const string Couch = "Levels/GrumpSpace/Couch";
+                
+                public static class Posters
+                {
+                    public const string Brian = "Levels/GrumpSpace/Posters/Brian";
+                    public const string Goose = "Levels/GrumpSpace/Posters/Goose";
+                    public const string Larry = "Levels/GrumpSpace/Posters/Larry";
+                    public const string NoUse = "Levels/GrumpSpace/Posters/NoUse";
+                    public const string UpDog = "Levels/GrumpSpace/Posters/UpDog";
+                }
+            }
+            
+            public static class Stage1_1
+            {
+                public const string BridgeBottom = "Levels/Stage1_1/BridgeBottom";
+                public const string BridgeRailing = "Levels/Stage1_1/BridgeRailing";
+                public const string stage = "Levels/Stage1_1/stage";
+                public const string Street = "Levels/Stage1_1/Street";
+            }
+            public const string stages = "Levels/stages";
+        }
+        
+        public static class Music
+        {
+            public const string Smash1 = "Music/Smash1";
+            public const string Smash2 = "Music/Smash2";
+        }
+        
+        public static class Shaders
+        {
+            public const string GaussianBlur = "Shaders/GaussianBlur";
+        }
+        
+        public static class Sprites
+        {
+            public const string Arin = "Sprites/Arin";
+            public const string ArinBomb = "Sprites/ArinBomb";
+            public const string ArinLemon = "Sprites/ArinLemon";
+            public const string Booper = "Sprites/Booper";
+        }
+        
+        public static class UI
+        {
+            
+            public static class GrumpCade
+            {
+                public const string _1Up = "UI/GrumpCade/_1Up";
+                public const string _2Up = "UI/GrumpCade/_2Up";
+                public const string _3Up = "UI/GrumpCade/_3Up";
+                public const string _4Up = "UI/GrumpCade/_4Up";
+                public const string Arin = "UI/GrumpCade/Arin";
+                public const string Arin_Overlay = "UI/GrumpCade/Arin_Overlay";
+                public const string Barry = "UI/GrumpCade/Barry";
+                public const string Barry_Overlay = "UI/GrumpCade/Barry_Overlay";
+                public const string Danny = "UI/GrumpCade/Danny";
+                public const string Danny_Overlay = "UI/GrumpCade/Danny_Overlay";
+                public const string Ross = "UI/GrumpCade/Ross";
+                public const string Ross_Overlay = "UI/GrumpCade/Ross_Overlay";
+                public const string Suzy = "UI/GrumpCade/Suzy";
+                public const string Suzy_Overlay = "UI/GrumpCade/Suzy_Overlay";
+            }
+            
+            public static class HUD
+            {
+                public const string Arin = "UI/HUD/Arin";
+                public const string Bars = "UI/HUD/Bars";
+                public const string Fire = "UI/HUD/Fire";
+            }
+            
+            public static class Logos
+            {
+                public const string GameGrumps = "UI/Logos/GameGrumps";
+                public const string GameGrumpsTransition = "UI/Logos/GameGrumpsTransition";
+                public const string SteamTrain = "UI/Logos/SteamTrain";
+            }
+            
+            public static class Names
+            {
+                public const string Arin_name = "UI/Names/Arin_name";
+                public const string Barry_name = "UI/Names/Barry_name";
+                public const string Danny_name = "UI/Names/Danny_name";
+                public const string Ross_name = "UI/Names/Ross_name";
+                public const string Suzy_name = "UI/Names/Suzy_name";
+            }
+            
+            public static class Pause
+            {
+                public const string barry_pause = "UI/Pause/barry_pause";
+                public const string paused_bubble = "UI/Pause/paused_bubble";
+            }
+            public const string SaveBack = "UI/SaveBack";
+        }
+    }
+}
+
